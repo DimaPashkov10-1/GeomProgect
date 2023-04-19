c7ecb8259dea3105f9682f2cf82d1338d4a549bf
dpashkov <dmitriy@pashkov.net>
2023-04-19 16:53:43 +0300

    render text

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,5 +1,6 @@
 package app;
 
+import controls.Label;
 import io.github.humbleui.jwm.*;
 import io.github.humbleui.jwm.skija.EventFrameSkija;
 import io.github.humbleui.skija.Canvas;
@@ -13,6 +14,7 @@ import java.io.File;
 import java.util.function.Consumer;
 
 import static app.Colors.APP_BACKGROUND_COLOR;
+import static app.Colors.PANEL_BACKGROUND_COLOR;
 
 /**
  * Класс окна приложения
@@ -30,6 +32,10 @@ public class Application implements Consumer<Event> {
      * радиус скругления элементов
      */
     public static final int C_RAD_IN_PX = 4;
+    /**
+     * Первый заголовок
+     */
+    private final Label label;
 
     /**
      * Конструктор окна приложения
@@ -37,6 +43,7 @@ public class Application implements Consumer<Event> {
     public Application() {
         // создаём окно
         window = App.makeWindow();
+        label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, "Привет, мир!");
         // задаём обработчиком событий текущий объект
         window.setEventListener(this);
         // задаём заголовок
@@ -45,6 +52,7 @@ public class Application implements Consumer<Event> {
         window.setWindowSize(900, 900);
         // задаём его положение
         window.setWindowPosition(100, 100);
+
         // задаём иконку
 
         switch (Platform.CURRENT) {
@@ -103,26 +111,15 @@ public class Application implements Consumer<Event> {
      * @param windowCS СК окна
      */
     public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
+
         // запоминаем изменения (пока что там просто заливка цветом)
         canvas.save();
         // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-
-        // восстанавливаем состояние канваса
-        canvas.restore();
-        // координаты левого верхнего края окна
-        int rX = windowCS.getSize().x / 3;
-        int rY = windowCS.getSize().y / 3;
-        // ширина и высота
-        int rWidth =  windowCS.getSize().x  / 3;
-        int rHeight = windowCS.getSize().y  / 3;
-        // создаём кисть
-        Paint paint = new Paint();
-        // задаём цвет рисования
-        paint.setColor(Misc.getColor(100, 255, 255, 255));
-        // рисуем квадрат
-        canvas.drawRRect(RRect.makeXYWH(rX, rY, rWidth, rHeight, 4), paint);
-
+        // рисуем заголовок
+        label.paint(canvas, windowCS);
+        // рисуем заголовок в точке [100,100] с шириной и выостой 200
+        label.paint(canvas, new CoordinateSystem2i(100, 100, 200, 200));
         // восстанавливаем состояние канваса
         canvas.restore();
     }
