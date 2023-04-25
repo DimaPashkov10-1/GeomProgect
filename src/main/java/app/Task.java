1e16f5c80ab87d3c28caeaafc47c3dbf0a4872d2
dpashkov <dmitriy@pashkov.net>
2023-04-25 16:13:45 +0300

    solve button

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -49,6 +49,10 @@ public class Task {
      * Последняя СК окна
      */
     private CoordinateSystem2i lastWindowCS;
+    /**
+     * Флаг, решена ли задача
+     */
+    private boolean solved;
 
     /**
      * Задача
@@ -98,6 +102,7 @@ public class Task {
      * @param pointSet множество
      */
     public void addPoint(Vector2d pos, Point.PointSet pointSet) {
+        solved = false;
         Point newPoint = new Point(pos, pointSet);
         points.add(newPoint);
         PanelLog.info("точка " + newPoint + " добавлена в " + newPoint.getSetName());
@@ -156,18 +161,28 @@ public class Task {
      */
     public void clear() {
         points.clear();
+        solved = false;
     }
     /**
      * Решить задачу
      */
     public void solve() {
+        solved = true;
         PanelLog.warning("Вызван метод solve()\n Пока что решения нет");
     }
     /**
      * Отмена решения задачи
      */
     public void cancel() {
-
+        solved = false;
+    }
+    /**
+     * проверка, решена ли задача
+     *
+     * @return флаг
+     */
+    public boolean isSolved() {
+        return solved;
     }
 
 }
